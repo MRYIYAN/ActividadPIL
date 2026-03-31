405aec2d4af0751af51baa032048e238cec6b1ce
ELACABAD0 <181473229+ELACABAD0@users.noreply.github.com>
2026-04-01 00:50:11 +0200

    [ADD] Creación de archivo geometria.c con estructura base del programa

---
+++ proyecto/src/geometria.c
@@ -0,0 +1,11 @@
+/**
+ * @file geometria.c
+ * @brief Funciones de cálculo geométrico.
+ *
+ * Incluye funciones para:
+ * - Distancia entre puntos
+ * - Área de triángulos
+ * - Perímetro
+ * - Baricentro
+ * - eje central de imprimirTriangulos.c
+ */
