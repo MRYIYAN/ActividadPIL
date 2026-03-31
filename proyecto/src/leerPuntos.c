12af60424316c5ef763307ee6786d7a6a9cc4d3b
ELACABAD0 <181473229+ELACABAD0@users.noreply.github.com>
2026-04-01 00:35:42 +0200

    [ADD] Creación archivo leerPuntos.C con estructura base del programa

---
+++ proyecto/src/leerPuntos.c
@@ -0,0 +1,6 @@
+/**
+ * @file leerPuntos.c
+ * @brief Funciones para leer puntos desde fichero.
+ *
+ * Contiene la implementación de la función leerPuntos, encargada de la gestión de memoria dinámica y lectura de datos.
+ */
