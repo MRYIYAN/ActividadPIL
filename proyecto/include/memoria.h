b8f41ccc97cb9a388fd0a07557b8d06faf0606e0
ELACABAD0 <181473229+ELACABAD0@users.noreply.github.com>
2026-04-01 01:03:45 +0200

    [ADD] Creación del archivo memoria.h con información base del programa
    Revisión de la información proporcionada para la ejecución de este archivo

---
+++ proyecto/include/memoria.h
@@ -0,0 +1,7 @@
+/**
+ * @file memoria.h
+ * @brief Funciones de gestión de memoria y ficheros.
+ *
+ * Declara las funciones auxiliares para:
+ * "Estructura de datos y sus funciones, por ejemplo constructor de puntos o triángulos"?
+ */
