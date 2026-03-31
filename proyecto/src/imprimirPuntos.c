15908766091cc0bc60f2b3be0e07b8d78f485b61
ELACABAD0 <181473229+ELACABAD0@users.noreply.github.com>
2026-04-01 00:38:48 +0200

    [ADD] Crear archivo imprimirPuntos.c con estructura base del programa

---
+++ proyecto/src/imprimirPuntos.c
@@ -0,0 +1,6 @@
+/**
+ * @file imprimirPuntos.c
+ * @brief Funciones de impresión de puntos.
+ *
+ * Muestra por consola la información de los puntos en formato estructurado (la cabecera y mostrar la tabla).
+ */
