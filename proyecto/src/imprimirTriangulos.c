f4f73b7adf6769e4f65c4f40c199e0e2a4152294
ELACABAD0 <181473229+ELACABAD0@users.noreply.github.com>
2026-04-01 00:47:23 +0200

    [ADD] Creación de imprimirTriangulos.c con la estructura base del programa

---
+++ proyecto/src/imprimirTriangulos.c
@@ -0,0 +1,9 @@
+/**
+ * @file imprimirTriangulos.c
+ * @brief Funciones de impresión de triángulos.
+ *
+ * Imprime:
+ * - Información básica
+ * - Área, perímetro y baricentro
+ * - Usará las funciones de geometría.c
+ */
