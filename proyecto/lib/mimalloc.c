d8e80274d1dba1ac908aad7936f4b993d96efc0d
ELACABAD0 <181473229+ELACABAD0@users.noreply.github.com>
2026-04-01 01:15:29 +0200

    [ADD] Añadir mimalloc.c con documentación inicial del programa

---
+++ proyecto/lib/mimalloc.c
@@ -0,0 +1,24 @@
+/**
+ * @file mimalloc.c
+ * @brief Reserva segura de memoria dinámica.
+ *
+ * Implementa una función para reservar memoria utilizando malloc.
+ * Si la reserva falla, el programa finaliza mostrando un mensaje de error.
+ * Si la reserva tiene éxito, devuelve un puntero al bloque de memoria reservado.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void *mimalloc(unsigned tam)
+{
+    void *ptr;
+
+    if ((ptr = malloc(tam)) == NULL) {
+        printf("\nError in miealloc:");
+        printf("\n Memory allocation failed! \n Bye...\n\n");
+        exit(1);
+    }
+    else
+        return ptr;
+}
