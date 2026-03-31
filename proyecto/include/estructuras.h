c2313cd2287ab73f7dd2fb68b81f0763bf40a4aa
ELACABAD0 <181473229+ELACABAD0@users.noreply.github.com>
2026-04-01 01:00:00 +0200

    [ADD] Crear archivo estructuras.h con información base del programa
    Revisión sobre la información proporcionada

---
+++ proyecto/include/estructuras.h
@@ -0,0 +1,6 @@
+/**
+ * @file estructuras.h
+ * @brief Definición de estructuras y prototipos del proyecto.
+ *
+ * Contiene la declaración de las funciones(de memoria) utilizadas en el programa.
+ */
