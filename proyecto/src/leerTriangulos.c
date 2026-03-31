5b02028cff14e07429f966bef03590fa364660e4
ELACABAD0 <181473229+ELACABAD0@users.noreply.github.com>
2026-04-01 00:44:07 +0200

    [ADD] Crear archivo leerTriangulos.c con estructura base del programa
    Revisión de los puntos.

---
+++ proyecto/src/leerTriangulos.c
@@ -0,0 +1,9 @@
+/**
+ * @file leerTriangulos.c
+ * @brief Funciones para leer triángulos desde fichero.
+ *
+ * Se encarga de:
+ * - Leer triángulos desde archivo
+ * - Enlazar los puntos mediante punteros
+ * - No me quedó claro lo siguiente en este punto: Además, guardar puntos y reservar la memoria.
+ */
