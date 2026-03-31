055b8fbf08f2f8ea9a878a54ad8c0afeb014b2e8
ELACABAD0 <181473229+ELACABAD0@users.noreply.github.com>
2026-04-01 00:32:58 +0200

    [ADD] Creación archivo main.c con documentación inicial del programa

---
+++ proyecto/src/main.c
@@ -0,0 +1,10 @@
+/**
+ * @file main.c
+ * @brief Programa principal del proyecto.
+ *
+ * Orquesta la ejecución del programa:
+ * - Lee los puntos desde fichero
+ * - Imprime los puntos
+ * - Lee los triángulos
+ * - Imprime los triángulos con sus propiedades
+ */
