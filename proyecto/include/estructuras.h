b7e501d8ba7a36757cb0308b865c1670194871bc
ELACABAD0 <181473229+ELACABAD0@users.noreply.github.com>
2026-04-02 23:07:58 +0200

    [ADD] Estructuras punto2D y triangulo

--- proyecto/include/estructuras.h
+++ proyecto/include/estructuras.h
@@ -4,3 +4,12 @@
  *
  * Contiene la declaración de las funciones(de memoria) utilizadas en el programa.
  */
+typedef struct{
+    double x, y;
+    unsigned ID;
+}punto2D;
+
+typedef struct{
+    punto2D *nodo1, *nodo2, *nodo3;
+    unsigned ID, capa;
+}triangulo;
